fd7da23ae0828769c536fa2c1b3e6a7d8517106d
sebates <batesseb@msu.edu>
2023-12-10 21:16:52 -0500

    commit test

---
+++ index.js
@@ -0,0 +1,17 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+/* Borrowed (blatantly copied) from https://www.w3schools.com/howto/howto_js_accordion.asp */
+
